7fb93dd64a046233eafe966de3098b38c5408750
wizba <williamrabs@gmail.com>
2022-03-10 15:30:04 +0200

    2 files modified

--- app/Country.jsx
+++ app/Country.jsx
@@ -1,32 +1,31 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
- function Country(props){
-
-    const {myCountry} =props
-  return(
+function Country(props) {
+  const { myCountry } = props;
+  return (
     <div className="card">
-                        <div className="el-card-item">
-                            <div className="el-card-avatar el-overlay-1">
-                                {/* Image goes here */}
-                                <img
-                                    src={myCountry?.flag}
-                                    alt="user"
-                                />
-
-                            </div>
-                            <div className="el-card-content">
-                                {/* Name goes here */}
-                                <h4 className="m-b-0">{myCountry?.name}</h4>{' '}
-                                {/* currency goes here */}
-                                <span class="text-muted">Capital {myCountry?.capital}</span>
-                                <div>
-                                    <label>Currency symbol :</label>
-                                    <span> ??</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-  )
+      <div className="el-card-item">
+        <div className="el-card-avatar el-overlay-1">
+          {/* Image goes here */}
+          <img src={myCountry?.flag} alt="user" />
+        </div>
+        <div className="el-card-content">
+          {/* Name goes here */}
+          <h4 className="m-b-0">{myCountry?.name}</h4>{' '}
+          {/* currency goes here */}
+          <span class="text-muted">Capital {myCountry?.capital}</span>
+          <div>
+            <label>Currency symbol :</label>
+            <span> ??</span>
+          </div>
+          <div>
+            <label>Total population :</label>
+            <span> {myCountry.population}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export {Country}
+export { Country };
